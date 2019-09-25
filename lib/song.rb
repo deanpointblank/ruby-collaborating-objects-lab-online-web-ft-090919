855a7c9862f53aae865ba9313cbd338a910550ef
Dean L Betty <deanlloyd64@gmail.com>
2019-09-25 03:15:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,10 @@ class Song
     name_artist = file.split(/( - |.mp3)/)
     name_artist[0] = Artist.new(name_artist[0])
     name_artist[1] = self.new(name_artist[1])
+    song
   end
   
+  def artist_name=(artist)
+    @artist = artist
+  end
 end
